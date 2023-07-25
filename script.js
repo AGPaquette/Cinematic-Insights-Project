1fc7979aa9ab49bbdf7cdb578c6d02fe177db0d6
adrian garcia <adrian.garcia.s.1607@gmail.com>
2023-07-24 19:21:11 -0700

    new files added

---
+++ script.js
@@ -0,0 +1,27 @@
+function main() {
+
+};
+
+function searchFilm() {
+
+};
+
+function addWatchList() {
+
+};
+
+function subtractWatchList() {
+
+};
+
+function backButton() {
+
+};
+
+function yourWatchList() {
+
+};
+
+function filmResults() {
+
+};
