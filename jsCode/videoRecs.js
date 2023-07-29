116c742599a079ecbb112248a826acdcd6fc445b
adrian garcia <adrian.garcia.s.1607@gmail.com>
2023-07-28 19:37:32 -0700

    new files added

--- jsCode/videoRecs.js
+++ jsCode/videoRecs.js
@@ -2,10 +2,12 @@
 //allowes for youtube videos to be played on the website using the iframe api connected to the html
 let player
 
-function onYouTubeIframeAPIReady(x){
-    player = new YT.Player(`player${x}`, {
-        height: 300,
-        width: 600,
+function onYouTubeIframeAPIReady() {
+    console.log("inside")
+    for (i = 0; i < 6; i++) {   
+    player = new YT.Player(`player${i}`, {
+        height: 200,
+        width: 300,
         videoId: "YAD0s9_kbU4",
         playerVars: {
             playersinline:1,
@@ -13,28 +15,31 @@ function onYouTubeIframeAPIReady(x){
             controls:1,
         }
     });
-};
+    };
+    };
 
 function filmResults() {
+    
     for (i = 0; i < 6; i++) {
         console.log("hello")
-        var videoContainer = document.createElement("div").setAttribute("id", `player-container${i}`);
+        var videoContainer = document.createElement("section");
+        videoContainer.setAttribute("id", `container-{i}`);
 
-        var button = document.createElement("button")
-        button.textContent = "+"
-        videoContainer.appendChild(button)
+        var text = document.createElement("p");
+        text.textContent = "film summary";
+        videoContainer.appendChild(text);
 
-        var text = document.createElement("p")
-        text.textContent = "film summary"
+        var button = document.createElement("button");
+        button.textContent = "+";
+        videoContainer.appendChild(button);
 
-        var video = document.createElement("div").setAttribute("id", `player${i}`)
-        videoContainer.appendChild(video)
+        var video = document.createElement("div");
+        video.setAttribute("id", `player${i}`)
+        videoContainer.appendChild(video);
 
-        videoContainer.appendChild(text)
-        filmParent.appendChild(videoContainer)
+        filmParent.appendChild(videoContainer);
 
-        onYouTubeIframeAPIReady(i)
-
+        };
     };
-};
+
 
