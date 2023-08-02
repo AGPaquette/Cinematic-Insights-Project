b9caffb076c6812a069cf7c2f0ead0753458f2b1
adrian garcia <adrian.garcia.s.1607@gmail.com>
2023-08-02 15:22:00 -0700

    added comments to the code as well as removing unned code

--- jsCode/filmSearch.js
+++ jsCode/filmSearch.js
@@ -1,34 +1,25 @@
 var searchBtn = document.querySelector("#indexSearch")
 var filmValue = document.getElementById("filmName")
 
-function searchFilm() { 
-    console.log("film")
-    var userFilm = filmValue.value
-    if (document.URL.includes("index.html")) {
-        window.location.href = "./htmlFiles/menu.html?film=" + userFilm;
-    }
-    else {
-        window.location.href = "./menu.html?film=" + userFilm;
-    };
-
-};
-
 function checkforValue () {
+    //makes sure that the input element has text before doing any calls
     if (filmValue.value == "") {
         filmValue.setAttribute("placeholder", "PLEASE ENTER A FILM OR SHOW")
     }
     else {
+        //sets the input text to the local stroage
+        localStorage.setItem("searched-film", filmValue.value)
+        //redirects the user to the menu html page
         if (document.URL.includes("index.html")) {
-            localStorage.setItem("searched-film", filmValue.value)
             window.location.href = "./htmlFiles/menu.html"
         }
+        //calls the openAiReccommendations function
         else{
-            localStorage.setItem("searched-film", filmValue.value)
             openAiRecommendations()
         };
     };
 };
 
-
+//adds functionality to the search button
 searchBtn.addEventListener("click", checkforValue);
 
