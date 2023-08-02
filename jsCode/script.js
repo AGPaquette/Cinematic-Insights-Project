4b54a06d264fd5b67da7fd8564849bb9a9240baa
adrian garcia <adrian.garcia.s.1607@gmail.com>
2023-08-02 12:31:56 -0700

    removed more unneded cod

--- jsCode/script.js
+++ jsCode/script.js
@@ -7,9 +7,3 @@ function backButton() {
     window.location.href = "./menu.html"
 
 };
-
-if (document.URL.includes("menu.html")) {
-    var film = document.location.search.split("=")[1];
-    localStorage.setItem("searched-film", film);
-    openAiRecommendations();
-};
